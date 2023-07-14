90fd50001fb76f1a5ccbed14c4bc4da455d330f7
Volodya Sargsyan <volsarg@gmail.com>
2023-07-14 17:52:34 +0400

    add Students modules

--- frontend/models.py
+++ frontend/models.py
@@ -14,3 +14,11 @@ class Teachers(models.Model):
 
 
 
+class Students(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=20)
+    date = models.DateField()
+    image = models.ImageField(upload_to="teacher_img")
+    email = models.EmailField(max_length=50, primary_key=True)
+    user_name = models.CharField(max_length=10)
+    password = models.CharField(max_length=250)
