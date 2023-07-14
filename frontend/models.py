e2f30cbad0f534b076433c9138c0c5a19ba20be0
Volodya Sargsyan <volsarg@gmail.com>
2023-07-14 17:15:54 +0400

    change password length

--- frontend/models.py
+++ frontend/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Teachers(models.Model):
     image = models.ImageField(upload_to="teacher_img")
     email = models.EmailField(max_length=50, primary_key=True)
     user_name = models.CharField(max_length=10)
-    password = models.CharField(max_length=30)
+    password = models.CharField(max_length=250)
 
 
 
